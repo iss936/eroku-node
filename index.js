b64eb5e96b5d578644091d7b4d36b6e46eb4b21b
Issa Soumare <isoumare@atixnet.fr>
2017-04-06 12:08:17 +0200

    real api + html

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const api = express();
 
+require('./settings')(api);             console.log('initializing settings...');
+require('./models')(api);               console.log('initializing models...');
+require('./actions')(api);              console.log('initializing actions...');
+require('./middlewares')(api);          console.log('initializing middlewares...');
+require('./routes')(api);               console.log('initializing routes...');
+
+require('./static')(api);               console.log('initializing static files...');
+
 api.listen(process.env.PORT);
+console.log(`API listening on port ${process.env.PORT}`);
 
-api.get('*', function(req, res, next) {
-    res.send('It works!');
-});
-
+require('./boot')(api);                 console.log('\n executing boot scripts...');
